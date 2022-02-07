1966ba02db8effbf5c93107b490e3252395a0d61
Violet kritika <60845749+priyanshukali@users.noreply.github.com>
2022-02-07 19:54:15 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+  const elem = document.querySelector('.highlight');
+  const homeMenu = document.querySelector('#home-page');
+  const aboutMenu = document.querySelector('#about-page');
+  const servicesMenu = document.querySelector('#services-page');
+  let scrollPos = window.scrollY;
+  // console.log(scrollPos);
+
+  // adds 'highlight' class to my menu items
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
+    servicesMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    servicesMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  }
+
+  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove('highlight');
+  }
+};
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector('.is-active');
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.remove('active');
+  }
+};
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
+
+let generateBtn = document.getElementById("generate-btn");
+let copyBtn = document.getElementById("copy-btn");
+let outputColor = document.getElementById("output-color");
+let outputCode = document.getElementById("output-code");
+let hexString = "0123456789abcdef";
+
+let randomColor = () => {
+    let hexCode = "#";
+    for( i=0; i<6; i++){
+        hexCode += hexString[Math.floor(Math.random() * hexString.length)];
+    }
+    return hexCode;
+}
+
+let generateGrad = () => {
+    let colorOne = randomColor();
+    let colorTwo = randomColor();
+    let angle = Math.floor(Math.random() * 360);
+    outputColor.style.background = `linear-gradient(${angle}deg, ${colorOne}, ${colorTwo})`;
+    outputCode.value = `background: linear-gradient(${angle}deg, ${colorOne}, ${colorTwo});`;
+}
+
+copyBtn.addEventListener("click", () => {
+    outputCode.select();
+    document.execCommand("copy");
+    alert("Code Copied To Clipboard");
+});
+
+generateBtn.addEventListener("click", generateGrad);
+window.onload = generateGrad;
